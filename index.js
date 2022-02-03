b4676490cde679cf6fdec960ad9911069b2b2eac
Austin Baik <baik.austin@gmail.com>
2022-02-02 19:23:56 -0800

    commit tree 72f1d4faf14c720c485134919daa91d1789ebc30

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
   // To pass the tests, don't forget to return your fetch!
-  
+
 }
 
 function renderBooks(books) {
@@ -12,6 +15,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks()
 });
